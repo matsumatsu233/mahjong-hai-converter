7c7594cb2575a3e89af4da5cce99f46816cb6608
Cui <zhen.cui@p43477.intra.rakuten.co.jp>
2017-06-14 10:01:55 +0900

    Change image to gif

--- sources/mj-tactics/dl.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-for i in {1..9}
-do
-  curl http://yabejp.web.fc2.com/mahjong/img/tile_m/m${i}.png -o m${i}.png
-  curl http://yabejp.web.fc2.com/mahjong/img/tile_m/p${i}.png -o p${i}.png
-  curl http://yabejp.web.fc2.com/mahjong/img/tile_m/s${i}.png -o s${i}.png
-  curl http://yabejp.web.fc2.com/mahjong/img/tile_m/j${i}.png -o j${i}.png
-done
